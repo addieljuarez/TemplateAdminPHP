cf1336efc7dc9588ed19ec0b320a420a4f27136a
Addiel <addieljuarez@gmail.com>
2013-12-21 16:00:07 -0600

    new application

---
+++ application/views/general/header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<meta name="description" content="template ABC"/>
+		<meta name="author" content="Addiel Juarez"/>
+
+		<link rel="shortcut icon" href=""/>
+		<!-- <link href="offcanvas.css" rel="stylesheet"> -->
+
+
+	  	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" />
+	  	<title>template</title>
+
+		<link href="/resource/css/bootstrap.css" rel="stylesheet"/>
+		<link href="/resource/css/bootstrap.min.css" />
+		<link href="/resource/css/bootstrap-theme.css" />
+		<link href="/resource/css/bootstrap-theme.min.css" />
+		
+		<script src="/resource/js/bootstrap.js"></script>
+		<script src="/resource/js/bootstrap.min.js"></script>
+		<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+
+		
+	</head>
+
+	<body>
+		<div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-collapse">
+						<span class="sr-only">
+							Toggle navigation
+						</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">
+						Principal de template
+					</a>
+				</div>
+
+				<div class="colapse navbar-colapse">
+					<ul class='nav navbar-nav'>
+						<li class='active'>
+							<a href="#">
+								Home
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								Quienes Somos?
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								Servicios
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								Clientes
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								Contacto
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+	
