4e6d9961d28401a71bf58b29927d48058434d33b
Addiel <addieljuarez@gmail.com>
2013-12-22 01:55:20 -0600

    main sample

--- application/views/general/header.php
+++ application/views/general/header.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<!-- border-style:solid; border-color:red; border-width:1px; position:absolute; -->
 <html>
 	<head>
 		
@@ -78,7 +79,7 @@
 
 
 
-		<div class='container'  style='border-style:solid; border-color:red; border-width:1px; position:absolute; top:50px; left:auto'><!--contenedor del body-->
+		<div class='container'  style='position:absolute; top:70px; left:100px'><!--contenedor del body-->
 			<div class='row row-offcanvas row-offcanvas-right'>
 				<div class='col-xs-12 col-sm-9'>
 					<p class='pull-right visible-xs'>
@@ -98,33 +99,92 @@
 							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 						</p>
 					</div>
-					<div class='row'>
+					<div class='row'><!--row-->
 						<div class='col-6 col-sm-6 col-lg-4'>
 							<h2>
 								Heading
 							</h2>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<p>
+								<a href="#" class='btn btn-default' role='button'>
+									detalles &raquo;
+								</a>
+							</p>
 						</div>
-					</div><!--row-->
+						<div class='col-6 col-sm-6 col-lg-4'>
+							<h2>
+								Heading
+							</h2>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<p>
+								<a href="#" class='btn btn-default' role='button'>
+									detalles &raquo;
+								</a>
+							</p>
+						</div>
+						<div class='col-6 col-sm-6 col-lg-4'>
+							<h2>
+								Heading
+							</h2>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<p>
+								<a href="#" class='btn btn-default' role='button'>
+									detalles &raquo;
+								</a>
+							</p>
+						</div>
+					</div><!--end.row-->
 				</div><!--span-->
 				<div class='col-xs-6 col-sm-3 sidebar-offcanvas' id='sidebar' role='navigation'>
 					<div class='list-group'>
 						<a href="#" class='list-group-item active'>
 							Login Clientes
 						</a>
-						<a href="#" class='list-group-item active'>
+						<a href="#" class='list-group-item'>
 							Login Shopers
 						</a>
-						<a href="#" class='list-group-item active'>
+						<a href="#" class='list-group-item'>
 							Shopers Alta
 						</a>
-						<a href="#" class='list-group-item active'>
+						<a href="#" class='list-group-item'>
 							Administrador
 						</a>
 					</div><!--listGroup-->
 				</div><!--span-->
 
 			</div>
-		</div><!--end. contenedor del titulo de la plantilla-->
+
+			<hr>
+			<footer>
+				<p>
+					&copy; Negoapps 2014
+				</p>
+			</footer>
+
+
+
+		</div><!--end. contenedor del body de la plantilla-->
 
 
 
